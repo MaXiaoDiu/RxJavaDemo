1e59ad4e7bfdac8274ccaee781e811d941c266ae
DESKTOP-G8CP8FK\cyy15 <chengyy@mxchip.com>
2018-04-13 17:15:41 +0800

    添加了RxJava的Scheduler几个用法.

--- app/src/main/java/com/flyswallow/rxjavademo/RxJavaOneSchedulerActivity.java
+++ app/src/main/java/com/flyswallow/rxjavademo/RxJavaOneSchedulerActivity.java
@@ -16,7 +16,45 @@ public class RxJavaOneSchedulerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_java_one_scheduler);
-        SchedulerOne();
+        SchedulerTwo();
+
+    }
+
+    private void SchedulerTwo() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Log.d( "rx_newThread" , Thread.currentThread().getName()  );
+                rx_Scheduler();
+            }
+        }).start();
+    }
+
+    private void rx_Scheduler() {
+
+        Observable.create(new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> subscriber) {
+                        Log.d( "==Log==rx_call" , Thread.currentThread().getName()  );
+
+                        subscriber.onNext( "dd");
+                        subscriber.onCompleted();
+                    }
+                })
+                .map(new Func1<String, String >() {
+                    @Override
+                    public String call(String s) {
+                        Log.d( "==Log==rx_map" , Thread.currentThread().getName()  );
+                        return s + "88";
+                    }
+                })
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.d( "==Log==rx_subscribe" , Thread.currentThread().getName()  );
+                    }
+                }) ;
     }
 
     private void SchedulerOne() {
@@ -25,7 +63,7 @@ public class RxJavaOneSchedulerActivity extends AppCompatActivity {
             @Override
             public void call(Subscriber<? super String> subscriber) {
 
-                Log.d("==rx_call",Thread.currentThread().getName());
+                Log.d("==Log==rx_call",Thread.currentThread().getName());
                 subscriber.onNext("dd");
                 subscriber.onCompleted();
 
@@ -35,7 +73,7 @@ public class RxJavaOneSchedulerActivity extends AppCompatActivity {
             @Override
             public String call(String s) {
 
-                Log.d( "==rx_map" , Thread.currentThread().getName()  );
+                Log.d( "==Log==rx_map" , Thread.currentThread().getName()  );
                 return s + "rx_map";
             }
         })
@@ -43,8 +81,9 @@ public class RxJavaOneSchedulerActivity extends AppCompatActivity {
             @Override
             public void call(String s) {
 
-                Log.d( "==rx_subscribe" , Thread.currentThread().getName()  );
+                Log.d( "==Log==rx_subscribe" , Thread.currentThread().getName()  );
             }
         });
     }
+
 }
